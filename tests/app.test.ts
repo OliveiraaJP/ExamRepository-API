00403cf5eacfbae67ace3a0a9e9af1a170bdcc30
Joao Paulo <oliveirajpufrj@gmail.com>
2022-07-20 14:32:52 -0300

    building tests folder

---
+++ tests/app.test.ts
@@ -0,0 +1,9 @@
+import supertest from "supertest";
+import app from "../src/app.js";
+
+describe("User tests signup/in", () => {
+    it('sum 2 + 2 gives 4',async () => {
+        const sum = 2 + 2
+        expect(sum).toStrictEqual(4)
+    })
+})
