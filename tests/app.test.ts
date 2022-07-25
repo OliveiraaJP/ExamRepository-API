16d54cc9d67fa9a987226f8e1ed7d9a3f68d5210
Joao Paulo <oliveirajpufrj@gmail.com>
2022-07-25 14:06:44 -0300

    feat: deploy on heroku

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -7,12 +7,13 @@ import * as testFactory from "./factories/testFactory.js"
 
 beforeEach(async () => {
     await prisma.$executeRaw`TRUNCATE TABLE users`
+    await prisma.$executeRaw`TRUNCATE TABLE tests`
 })
 
 describe("User Tests", () => {
     it("Create a user", async () => {
         const login = userFactory.createUser()
-        const response = await supertest(app).post(`/signup`).send(login)
+        const response = await supertest(app).post(`/sign-up`).send(login)
         expect(response.statusCode).toBe(201)
 
         const user = await prisma.user.findFirst({
@@ -25,15 +26,15 @@ describe("User Tests", () => {
 
     it("Create a duplicate user", async () => {
         const login = userFactory.createUser()
-        await supertest(app).post(`/signup`).send(login)
-        const response = await supertest(app).post(`/signup`).send(login)
+        await supertest(app).post(`/sign-up`).send(login)
+        const response = await supertest(app).post(`/sign-up`).send(login)
         expect(response.statusCode).toBe(400)
     })
 
     it("Login a user", async () => {
         const login = userFactory.createUser()
         await supertest(app).post(`/signup`).send(login)
-        const response = await supertest(app).post(`/signin`).send({
+        const response = await supertest(app).post(`/sign-in`).send({
             email: login.email,
             password: login.password
         })
@@ -44,13 +45,17 @@ describe("User Tests", () => {
 
 describe("Create test Tests", () => {
     it("Create a test", async () => {
+        const login = userFactory.createUser()
+        const loginData = await supertest(app).post(`/sign-up`).send(login)
+        const token = loginData.body.token
         const test = testFactory.createTest()
-        const response = await supertest(app).post(`/tests`).send(test)
+        const response = await supertest(app).post(`/tests`).send(test).set("Authorization", `Bearer ${token}`)
         expect(response.statusCode).toBe(201)
     })
 })
 
 afterAll(async () => {
     await prisma.$executeRaw`TRUNCATE TABLE users`
+    await prisma.$executeRaw`TRUNCATE TABLE tests`
     await prisma.$disconnect()
 })
